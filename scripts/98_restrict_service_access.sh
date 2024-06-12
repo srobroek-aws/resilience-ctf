4ebebec3b4e3ca6172c3259e3085ea3434c4dfca
srobroek-aws <sjors@amazon.com>
2024-06-12 12:58:10 +0400

    moved fis restrictions to setup script, restricted access to cloud9

--- scripts/98_restrict_service_access.sh
+++
@@ -1,2 +0,0 @@
-aws iam create-policy --policy-name deny-fis --policy-document file://../iam/deny-fis-policy.json
-aws iam attach-role-policy --role-name WSParticipantRole --policy-arn arn:aws:iam::$ACCOUNT_ID:policy/deny-fis
