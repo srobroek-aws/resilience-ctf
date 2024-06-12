f584cbe5f623f04715f94c8a6dbc2b3ac5630d8d
srobroek-aws <sjors@amazon.com>
2024-06-12 12:36:47 +0400

    fixed deny service script to use correct account id

--- scripts/98_restrict_service_access.sh
+++ scripts/98_restrict_service_access.sh
@@ -1,2 +1,2 @@
-aws iam create-policy --policy-name deny-fis --policy-document file://../iam/deny-fis-policy
-aws iam attach-role-policy --role-name WSParticipantRole --policy-arn arn:aws:iam::aws:policy/deny-fis
+aws iam create-policy --policy-name deny-fis --policy-document file://../iam/deny-fis-policy.json
+aws iam attach-role-policy --role-name WSParticipantRole --policy-arn arn:aws:iam::$ACCOUNT_ID:policy/deny-fis
